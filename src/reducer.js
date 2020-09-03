6ab8e7d7469f0a0bf541e0e4dddf76252252c2a2
zakariabijoy <zakaria@vonome.com>
2020-09-04 04:03:28 +0600

    creating a reducer

---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+let lastId = 0;
+
+function reducer(state = [], action) {
+    switch (action.type) {
+        case 'bugAdded':
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+
+            break;
+
+        case 'bugRemoved':
+            return [
+                ...state,
+                {
+                    id: ++lastId,
+                    description: action.payload.description,
+                    resolved: false
+                }
+            ];
+            break;
+
+        default:
+            return state;
+            break;
+    }
+    // if (action.type === 'bugAdded')
+    //     return [
+    //         ...state,
+    //         {
+    //             id: ++lastId,
+    //             description: action.payload.description,
+    //             resolved: false
+    //         }
+    //     ];
+    // else if (action.type === 'bugRemoved')
+    //     return state.filter(bug => bug.id !== action.payload.id);
+
+    // return state;
+}
