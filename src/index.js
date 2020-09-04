ba5298a66fddf3865d23ed79e28b3032ce5d8ffb
zakariabijoy <zakaria@vonome.com>
2020-09-05 03:15:14 +0600

    create actions and reducer for project using createSlice of redux tool kit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import * as actions from './store/projects';
 
 
 const store = configureStore();
@@ -7,12 +7,14 @@ const store = configureStore();
 const unsubscribe = store.subscribe(() => {
     console.log("store changed!", store.getState());
 });
-store.dispatch(actions.bugAdded({ description: 'Bug 1' }));
-store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
-store.dispatch(actions.bugAdded({ description: 'Bug 3' }));
-store.dispatch(actions.bugAdded({ description: 'Bug 4' }));
+// store.dispatch(actions.bugAdded({ description: 'Bug 1' }));
+// store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
+// store.dispatch(actions.bugAdded({ description: 'Bug 3' }));
+// store.dispatch(actions.bugAdded({ description: 'Bug 4' }));
 
-store.dispatch(actions.bugResolved({ id: 1 }));
+// store.dispatch(actions.bugResolved({ id: 1 }));
+
+store.dispatch(actions.projectAdded({ name: 'project 1' }));
 
 unsubscribe();
 
