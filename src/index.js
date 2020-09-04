b26a6821a7f141b3b956dccfdd5946fe2d992671
zakariabijoy <zakaria@vonome.com>
2020-09-05 00:02:28 +0600

    installing redux-devtools-extension package to communicate with browser extension

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const unsubscribe = store.subscribe(() => {
     console.log("store changed!", store.getState());
 });
 store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugAdded('Bug 2'));
+store.dispatch(bugAdded('Bug 3'));
+store.dispatch(bugAdded('Bug 4'));
+
 store.dispatch(bugresolved(1));
 
 unsubscribe();
