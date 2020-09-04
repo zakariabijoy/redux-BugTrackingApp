c73089609c3ea73e157fc76014179bb73fca6862
zakariabijoy <zakaria@vonome.com>
2020-09-05 02:51:01 +0600

    creating reducer  using redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 
 
@@ -23,44 +23,18 @@ export const bugRemoved = createAction("bugRemoved");
 // reducer
 
 let lastId = 0;
-
-export default function reducer(state = [], action) {
-    switch (action.type) {
-        case bugAdded.type:
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false
-                }
-            ];
-
-            break;
-
-        case bugRemoved.type:
-            return state.filter(bug => bug.id !== action.payload.id);
-            break;
-
-        case bugResolved.type:
-            return state.map(bug => bug.id !== action.payload.id ? bug : { ...bug, resolved: true });
-            break;
-
-        default:
-            return state;
-            break;
+export default createReducer([], {
+    [bugAdded.type]: (bugs, action) => {
+        bugs.push({
+            id: ++lastId,
+            description: action.payload.description,
+            resolved: false
+        });
+    },
+    [bugResolved.type]: (bugs, action) => {
+        const index = bugs.findIndex(bug => bug.id === action.payload.id);
+        bugs[index].resolved = true;
     }
-    // if (action.type === 'bugAdded')
-    //     return [
-    //         ...state,
-    //         {
-    //             id: ++lastId,
-    //             description: action.payload.description,
-    //             resolved: false
-    //         }
-    //     ];
-    // else if (action.type === 'bugRemoved')
-    //     return state.filter(bug => bug.id !== action.payload.id);
 
-    // return state;
-}
+});
+
