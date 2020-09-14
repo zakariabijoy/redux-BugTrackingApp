cc092da3773db6c94bc25ed0f972a55b2b03bae1
zakariabijoy <zakaria@vonome.com>
2020-09-15 00:19:06 +0600

    Mocking Http Calls by axios-mock-adapter -D for unit test

--- src/store/test/bugs.spec.js
+++ src/store/test/bugs.spec.js
@@ -24,17 +24,23 @@
 
 
 // Social tests
-
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addBug } from '../bugs';
 import configureStore from '../configureStore';
 
 describe("bugSlice", () => {
     it("should handle the addBug action", async () => {
-        const store = configureStore();
         const bug = { description: 'a' };
+        const savedBug = { ...bug, id: 1 };
+
+        const fakeAxios = new MockAdapter(axios);
+        fakeAxios.onPost('/bugs').reply(200, savedBug);
+
+        const store = configureStore();
         await store.dispatch(addBug());
 
-        expect(store.getState().entities.bugs.list).toHaveLength(1);
+        expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
 
     });
 });
