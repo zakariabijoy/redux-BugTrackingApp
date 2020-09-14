a2941555333a223bde8553e830a67e1628c12cb0
zakariabijoy <zakaria@vonome.com>
2020-09-15 01:03:36 +0600

    added unit test for getUnresolvedBugs selectors

--- src/store/test/bugs.spec.js
+++ src/store/test/bugs.spec.js
@@ -26,8 +26,9 @@
 // Social tests
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { addBug } from '../bugs';
+import { addBug, getUnresolvedBugs } from '../bugs';
 import configureStore from '../configureStore';
+
 
 describe("bugSlice", () => {
 
@@ -39,6 +40,14 @@ describe("bugSlice", () => {
     });
 
     const bugsSlice = () => store.getState().entities.bugs;
+
+    const createState = () => ({
+        entities: {
+            bugs: {
+                list: []
+            }
+        }
+    });
 
     it("should add the bug to the store if it's saved to the server", async () => {
         // AAA
@@ -69,5 +78,16 @@ describe("bugSlice", () => {
         expect(bugsSlice().list).toHaveLength(0);
 
 
+    });
+
+    describe('selectors', () => {
+        it("getUnresolvedBugs", () => {
+            const state = createState();
+            state.entities.bugs.list = [{ id: 1, resolved: true }, { id: 1 }, { id: 1 }];
+
+            const result = getUnresolvedBugs(state);
+
+            expect(result).toHaveLength(2);
+        });
     });
 });
